e4a49173df54066341322350d770e4e94bfda5b0
hahabread <64178002+smolkiwii@users.noreply.github.com>
2022-03-05 17:07:37 -0500

    Created Larger Enemy and Bullets
    Chonkers

---
+++ Jammies 4/Assets/BigBullet.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBullet : MonoBehaviour
+{
+    
+    public float speed = 10.0f;                 //bullet speed   
+    private Rigidbody2D rb2d;                   //create rigidbody component  
+    private Vector3 move = Vector3.down;        //create a vector that only moves upwards (enemy's bullet can only move in one direction)
+
+    public void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //triggered on collision. param is what this collided with
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the bullet
+        transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        //if the bullet moved, update the rigidbody to follow it
+        if (move != Vector3.zero)
+        {
+            Vector3 translation = move * speed * Time.fixedDeltaTime;
+            Vector3 newPosition = transform.position + translation;
+            rb2d.MovePosition(newPosition);
+        }
+    }
+}
---
+++ Jammies 4/Assets/ChonkyEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChonkyEnemy : MonoBehaviour
+{
+
+    private float fireRate = 0.8f;                      //firerate
+    private float fireTimer = 0f;                       //timer for shooting
+    public GameObject bullet;                           //bullet object to be fired
+    private Rigidbody2D rb2d;                           //Create rigidbody
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fire (firerate) bullets per second
+        fireTimer += Time.deltaTime;
+        if(fireTimer > 1 / fireRate)
+        {
+            fireTimer = 0;
+            Instantiate(bullet, transform.position, transform.rotation * Quaternion.Euler(180f, 0f, 0f));
+        }
+    }
+}
