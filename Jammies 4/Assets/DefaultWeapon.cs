224f0054a5b3ffe5937c9bf3f6363d5fa696386e
hahabread <64178002+smolkiwii@users.noreply.github.com>
2022-03-06 00:07:20 -0500

    Weapon Changes

--- Jammies 4/Assets/DefaultWeapon.cs
+++ Jammies 4/Assets/DefaultWeapon.cs
@@ -6,7 +6,6 @@ public class DefaultWeapon : Weapon
 {    
     public override void fire(Player player, GameObject bullet)
     {
-        Debug.Log("firing from weapon");
-        GameObject instance = GameObject.Instantiate(bullet, player.transform.position, player.transform.rotation) as GameObject;
+        GameObject.Instantiate(bullet, player.transform.position, player.transform.rotation);
     }
 }
